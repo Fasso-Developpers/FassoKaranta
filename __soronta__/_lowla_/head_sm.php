3cd7ee5f812544648d758aa8235c74a4435e9dd5
Lancine KANTE <kantemou@gmail.com>
2015-09-01 01:33:15 +0100

    Move css for nko/ and friend to head_sm.php file

---
+++ __soronta__/_lowla_/head_sm.php
@@ -0,0 +1,15 @@
+<?php
+	echo('
+	<link rel="icon" href="images/fassoIcone.ico" type="image/x-icon">
+	<!-- styles css -->
+	<link rel="stylesheet" href="../_css_/styles_header.css" />
+	<link rel="stylesheet" href="../_css_/styles_contenu_full.css" />
+	<link rel="stylesheet" href="../_css_/styles_contenu_para.css" />
+	<link rel="stylesheet" href="../_css_/styles_contenu_titre.css" />
+	<link rel="stylesheet" href="../_css_/styles_footer.css" />
+
+	<!-- script javascript -->
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js"></script>
+	<script src="../_js_/site.js"></script>
+	');
+?>
