b11e6d8154ec4dbd059f1779a066ab5e69cc6eb8
Lancine KANTE <kantemou@gmail.com>
2015-09-04 01:46:58 +0100

    Update of connect.php to new connexion files

--- __soronta__/_lontabdo_/connect.php
+++ __soronta__/_lontabdo_/connect.php
@@ -4,11 +4,12 @@
 	{
 		echo "Error occured while connecting with database".mysqli_connect_error();
 	}
-
+	
+	try {
+	    $db = new PDO('mysql:host=localhost;dbname=fasso_karanta', 'root', '');
+	    $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+	} catch (PDOException $e) {
+	    $error = $e->getMessage();
+	}
 	session_start();
-	
-	// Registered table
-	define("RTABLE", "registred");
-	define("LTABLE", "nqo_level");
-	
 ?>
