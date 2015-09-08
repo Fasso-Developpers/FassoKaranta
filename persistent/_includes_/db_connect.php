43a8a04550b1198107628b3c714c85f3dd941952
Lancine KANTE <kantemou@gmail.com>
2015-09-08 02:16:04 +0100

    Remove persistent/ as login to projet

--- persistent/_includes_/db_connect.php
+++
@@ -1,7 +0,0 @@
-<?php
-try {
-    $db = new PDO('mysql:host=localhost;dbname=fasso_karanta', 'root', '');
-    $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-} catch (PDOException $e) {
-    $error = $e->getMessage();
-}
