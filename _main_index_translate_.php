43f11eb1c1b064ff562946f2f7d32a1941b7c8fb
Lancine KANTE <kantemou@gmail.com>
2015-09-15 17:50:47 +0100

    Add index translate page for main index

---
+++ _main_index_translate_.php
@@ -0,0 +1,83 @@
+<?php
+//change interface in 'nko', 'en', 'fr';
+	$show_lang = 'en';
+
+	if (isset($_GET['lang'])) {
+		$show_lang = $_GET['lang'];
+	}elseif (isset($_POST['lang'])) {
+		$show_lang = $_POST['lang'];
+	}
+
+	define('LABEL_LANG', $show_lang);
+
+     $nko = array(
+     			'page_title' 	=> "ߝߊ߬ߛߏ ߞߊ߬ߙߊ߲߬ߕߊ", 
+     			'main_welcome' 	=> "ߊߟߎ߫ ߣߌ߫ ߛߣߍ߫ ߝߊ߬ߛߏ ߞߊ߬ߙߊ߲߬ߕߊ ߟߊ߫ ߹", 
+     			'on_fasso' 		=> "ߊߟߎ߫ ߦߋ߫ ߝߊ߬ߛߏ ߞߊ߬ߙߊ߲߬ߕߊ ߟߋ߬ ߟߊ߫ ߣߌ߲߬߸ ߊߟߎ߫ ߣߌ߫ ߛߣߍ߫. ߒߞߏ ߛߙߊ߫ ߘߐ߫߸ ߊߟߎ߫ ߘߌ߫ ߞߏ߫ ߛߌߦߊߡߊ߲߫ ߟߐ߲߫ ߦߊ߲߬", 
+     			'for_example' 	=> "ߟߊߒߡߊ߫ ߘߐ߫", 
+     			'nqo' 			=> "ߒߞߏ", 
+     			'nqo_desc'		=> "ߌ ߓߘߊ߫ ߒߞߏ ߞߏߡߍ߲߫ ߓߊ߬؟ ߣߴߌ ߡߊ߫ ߊ߬ ߞߏߡߍ߲߫ ߝߟߐ߫߸ ߏ߬ ߕߍ߫ ߖߎ߯ߡߊ߲߫ ߘߌ߫. ߒ߬ߓߊ߬ ߌ ߣߌ߫ ߛߣߍ߫ ߒߞߏ ߞߊߙߊ߲߫ ߦߙߐ߫ ߓߟߏߡߊߞߊ߬ߣߍ߲ ߘߐ߫.
+								  ߦߙߐ ߣߌ߲߬ ߘߌ߫ ߒߞߏ ߛߓߍߟߌ ߕߝߐ߬ߦߊ߬ ߊߟߎ߫ ߢߍ߫. ߊ߲ ߞߎߡߊߕߐ߫ ߞߊߓߋ ߟߎ߬ ߓߍ߯ ߟߋ߬ ߞߊ߲߬ ߞߵߊ߬ ߘߊߡߌ߬ߣߊ߬ ߛߓߍߛߎ߲ ߡߊ߬ ߞߊ߬ ߥߊ߫ ߊ߬ ߟߊߞߍ߫ ߡߊ߲߬ߕߊ߬ߦߊ ߡߊ߬", 
+     			'computer'		=> "ߕߟߋ߬ߓߊ߬ߦߊ", 
+     			'computer_desc' => "ߕߎ߬ߡߊ߬ߘߐ߫ ߕߟߋ߬ߓߊ ߥߟߊ߫ ߜߋߟߋ߲ߜߋߟߋ߲ ߘߏ߫ ߦߋ߫ ߌ ߓߟߏ߫ ߣߌ߲ߛߊ߲߬ ߞߵߊ߬ ߕߘߍ߬ ߌ ߡߊ߫ ߊ߬ ߣߊ߬ߝߊ ߓߍ߯ ߟߐ߲߫.
+								 ߒ߬ߓߊ߬ ߦߙߐ ߣߌ߲߬ ߘߴߊߟߎ߫ ߘߍ߬ߡߍ߲߬ ߊߟߎ߫ ߟߊ߫ ߞߍߟߊ߲ ߏ߬ ߟߎ߬ ߕߣߐ߬ߓߐ߬ ߟߊ߫ ߊ߬ ߢߊ߫ ߓߘߍ ߡߊ߬. ߓߌ߬ ߕߟߋ ߣߌ߲߬ ߟߐ߲ߞߏ ߢߊ߰ߒ߬ߞߐ߬ߓߊߟߌ ߡߍ߲ ߕߐ߯ ߞߏ߫ ߕߟߋ߬ߓߊ߬ߦߊ߸ ߊ߲ ߘߌ߫ ߏ߬ ߥߟߊ߬ߘߊ߬ ߢߣߊߡߊ ߟߎ߬ ߘߴߊߟߎ߫ ߡߊ߬.
+								 ߖߐ߲߰ߛߊ߫ ߊߟߎ߫ ߘߌ߫ ߏ߬ ߢߊߦߋ߫ ߖߏߣߊ߫ ߡߊ߲߬ߘߋ߫ ߝߘߏ߬ߓߊ߬ ߛߓߍߛߎ߲ ߘߐ߫. ߣߴߊߟߎ߫ ߞߊ߬ ߊ߲ ߥߟߊ߬ߘߊ ߡߍ߲߬ ߠߎ߫ ߕߊ߬ ߊߟߎ߫ ߕߍ߫ ߞߍ߫ ߟߏߟߊ߲߫ ߘߌ߫ ߕߟߋ߬ߓߊ߬ ߞߏ ߣߌ߫ ߜߋߟߋ߲ߜߋߟߋ߲߫ ߞߏ ߛߌ߫ ߘߐ߫ ߡߎ߬ߕߎ߲߬", 
+     			'game' 			=> "ߕߏߟߏ߲", 
+     			'game_desc' 	=> "ߕߋ߲߬ߋ߲߫ߕߋ߲߬߸ ߕߏߟߏ߲ ߠߎ߬ ߞߊ߫ ߛߌߦߊ߫ ߓߊ ߟߋ߬. ߡߐ߰ ߛߌߦߊߡߊ߲߫ ߖߍ߬ߘߍ ߦߋ߫ ߕߎ߬ߡߊ߬ ߛߌߦߊߡߊ߲߫ ߞߍ߫ ߟߊ߫ ߊ߬ߟߎ߬ ߕߏߟߏ߲߫ ߠߊ߫.
+								 ߒ߬ߓߊ߬ ߊ߲ߠߎ߬ ߦߋ߫ ߡߊ߲߬ߘߋ߲߬ ߖߏ߯ߘߏ߲ ߕߏߟߏ߲ ߏ߬ ߟߎ߫ ߟߋ߫ ߓߌ߬ߟߊ߬ ߞߏ߫ ߘߏ߫ ߝߊ߬ߛߏ߬ߘߋ߲ ߠߎ߫ ߛߊ߬ߥߏ ߘߐ߫ ߦߙߐ ߣߌ߲߬ ߘߐ߫", 
+     			'culture' 		=> "ߟߐ߲ߠߌߦߊ", 
+     			'culture_desc'	=> "ߡߊ߲߬ߘߋ߲߬ ߡߊߟߐ߲ߣߍ߲߫ ߊ߬ ߟߐ߲ߠߌߦߊ߫ ߥߙߍߓߊ ߟߋ߫ ߡߊ߬.
+								 ߊ߬ ߟߊ߫ ߛߌߦߊߦߊ ߡߊ߬߸ ߊ߲ ߡߊ߫ ߡߍ߲ ߠߎ߬ ߟߐ߲߫ ߡߎߣߎ߲߬ ߊߟߎ߫ ߘߌ߫ ߘߌ߬ߢߍ߬ ߞߵߏ߬ ߟߎ߬ ߟߊߛߋ߫ ߊ߲ ߡߊ߬. ߏ߬ ߞߍߕߐ߫ ߒߞߏ ߟߋ߬ ߘߐ߫. ߏ߬ ߘߴߊߟߎ߫ ߘߍ߬ߡߍ߲߬ ߛߓߍߟߌ ߣߌ߫ ߘߐ߬ߞߊ߬ߙߊ߲߬ߠߌ߲ ߠߊߢߊ߬ ߟߊ߫ ߒߞߏ ߘߐ߫
+								 ߊ߬ ߘߌ߫ ߛߊߓߎ߫ ߢߌߡߊ߫ ߘߌ߫ ߞߵߊߟߎ߫ ߟߊ߫ ߞߊߓߋ ߟߎ߬ ߡߊߝߙߊ߬ ߞߊ߬ ߛߓߍߟߌ ߣߌ߫ ߘߐ߬ߞߊ߬ߙߊ߲߬ߠߌ ߠߎ߬ ߓߍ߯ ߟߊߖߐߟߐ߲߫ ߏ߬ ߘߐ߫ ߛߍ߬ߣߍ߲߬ ߖߟߐߟߐߟߐ߫.
+								ߊ߲ ߞߊ߬ ߝߋ߲߫ ߺ ߋ ߺ ߝߋ߲߫ ߟߐ߲߫ ߟߐ߲ߠߌߦߊ ߏ߬ ߟߎ߬ ߘߐ߫߸ ߊ߲ ߘߌ߫ ߏ߬ ߓߍ߯ ߟߊߛߋ߫ ߊߟߎ߫ ߡߊ߬ ߣߐ߰ߦߊ ߘߝߊߣߍ߲ ߘߐ߫", 
+				'souhait' 		=> "ߊߟߎ߫ ߣߌ߫ ߘߐ߬ߞߊ߬ߙߊ߲߬ߠߌ ߢߌߡߊ߫ ߹",
+				);
+	$en = array(
+     			'page_title' 	=> "Fasso school", 
+     			'main_welcome' 	=> "Welcome on Fasso school !", 
+     			'you_are_on' 	=> "You are in Fasso school, here you can learn everythings in N'ko", 
+     			'for_example' 	=> "For example", 
+     			'nqo' 			=> "N'ko", 
+     			'nqo_desc'		=> "This part explain n'ko, if you are never learn n'ko, it is not bad because 
+										I will help you to anderstand N'ko in small delay.", 
+     			'computer'		=> "Computer science", 
+     			'computer_desc' => "You have a computer but you can't use it very well,
+										you are welcome to learn the fundamentals about computer.", 
+     			'game' 			=> "Game", 
+     			'game_desc' 	=> "How many game do you know in Manden, if you don't know anything, 
+										in this section will learn very much.", 
+     			'culture' 		=> "Culture", 
+     			'culture_desc'	=> "After know the basic notion in N'ko, you must pratice to anderstand,
+										in this section, you will be the excellent reader I hope.", 
+				'souhait' 		=> "Good reading !",
+				);
+	$fr = array(
+     			'page_title' 	=> "Fasso Ecole", 
+     			'main_welcome' 	=> "Bienvenue à l'école de Fasso !", 
+     			'you_are_on' 	=> "Vous êtes dans l’école de Fasso, 
+										travers N’ko vous allez apprendre beaucoup de chose ici", 
+     			'for_example' 	=> "Par exemple", 
+     			'nqo' 			=> "N'ko", 
+     			'nqo_desc'		=> "Avez-vous vu l’écriture N’ko au paravent, oui ou non. 
+										Si non soyez la bienvenue dans la découverte et l’apprentissage de N’ko. 
+										Cette partie vous expliquera dans tout son détail l’écriture N’ko. 
+										Tous les niveaux seront abordés depuis l’alphabet jusqu’à l’orthographe.", 
+     			'computer'		=> "Informatique", 
+     			'computer_desc' => "Vous avez probablement un ordinateur, 
+										mais vous ne savez pas l’utiliser comme il le faut ; la section informatique 
+										vous permettra d’avoir les bases fondamentales en informatique.", 
+     			'game' 			=> "Jeux", 
+     			'game_desc' 	=> "Actuellement, les jeux informatiques sont très rependus, 
+										il y a certaines personnes qui passent beaucoup de temps derniers les appareils pour 
+										ces jeux. Nous allons mettre à la disposition de la nation mandingue 
+										nos jeux mandingues très éducatifs pour permettre un rapprochement à nous même.", 
+     			'culture' 		=> "Culture", 
+     			'culture_desc'	=> "La riche culture mandingue sera expliquée dans cette partie. 
+										Cette culture étant très vague, vous allez vous-même nous apporter 
+										certains éléments de celle-ci pour un accès libre à tous. 
+										Vous allez faire cela en LISANT et en ECRIVANT.
+										Vous allez comme ça approfondir vos connaissances en LECTURE et à ECRITURE 
+										en N’ko de manière rapide et efficace.", 
+				'souhait' 		=> "Bonne lecture !", 						
+				);
+?>
--- index.php
+++ index.php
@@ -1,8 +1,13 @@
 <?php
 	include_once("__soronta__/flo.php");
 	is_not_login();
-	$error = "";
-	$succes = "";
+	
+	// Information to translate the page for user
+	$user_id = $_SESSION['user_id'];
+	$kan_array = get_kan($user_id, $con);
+	$_POST['lang'] = $kan_array['kan'];
+	
+	include_once("_main_index_translate_.php");
 	define("MAIN_PAGE", "main_index");
 	define("CHAPITRE", "");
 ?>
@@ -11,7 +16,7 @@
 <html dir="auto">
 <head>
 	<meta charset="utf-8" />
-	<title>Fasso school</title>
+	<title><?php echo trad_lang('page_title'); ?></title>
 	<link rel="icon" href="http://www.fasso.org/karanta/_djiya_/fassoIcone.ico" type="image/x-icon">
 	<!--[if IE]><link rel="shortcut icon" type="image/x-icon" 
 		href="http://www.fasso.org/karanta/_djiya_/fassoIcone.ico" /><![endif]-->
@@ -28,7 +33,7 @@
 	
 </head>
 
-<body dir="auto">
+<body <?php align_by_lang('right','left'); ?> >
 
 <!-- Entête -->
 	
@@ -40,46 +45,45 @@
 		
 
 <!-- Cette partie contient le contenu de la page -->
-		<div dir="ltr" class="contenu" class="ombre">
+		<div <?php align_by_lang('right','left'); ?> class="contenu" class="ombre">
 
 			<!-- Titre -->
 			<?php include("__soronta__/_lowla_/contenu_titre_main.php"); ?>
 			
 			<!-- Paragraphe texte -->
 				<div id="paraTitre" class="rectangle ombre">
-					<h1 class="rectangle">Welcome !</h1>
+					<h1 <?php align_by_lang('right','left'); ?>><?php echo trad_lang('main_welcome'); ?></h1>
 				</div>
 			<div id="paraTexte" class="rectangle ombre">
 				<section id="intro">
 					<p style="text-align:center;">
-						You are in Fasso school, here you can learn everythings in N'ko. For example
+						<?php echo trad_lang('on_fasso'); ?>
 					</p>
+					
+					<p style="margin:0 20px 0 20px;" <?php align_by_lang('right','left'); ?>>
+						<?php echo trad_lang('for_example'); ?></p>
 
-					<ul id="listDmalon">
-						<li> <a href="nko/index.php">N'ko</a> 
-							<p class="def">(This part explain n'ko, if you are never learn n'ko, it is not bad because 
-								I will help you to anderstand N'ko in small delay.)</p>
+					<ul id="listDmalon" <?php align_by_lang('right','left'); ?>>
+						<li> <a href="nko/index.php"><?php echo trad_lang('nqo'); ?></a> 
+							<p class="def">(<?php echo trad_lang('nqo_desc'); ?>)</p>
 						</li>
-						<li> <a href="computer/index.php">Computer science</a> 
-							<p class="def">(You have a computer but you can't use it very well,
-								you are welcome to learn the fundamentals about computer.)</p>
+						<li> <a href="computer/index.php"><?php echo trad_lang('computer'); ?></a> 
+							<p class="def">(<?php echo trad_lang('computer_desc'); ?>)</p>
 						</li>
-						<li> <a href="game/index.php">Games</a> 
-							<p class="def">(How many game do you know in Manden, if you don't know anyone, 
-								this section will learn very much.)</p>
+						<li> <a href="game/index.php"><?php echo trad_lang('game'); ?></a> 
+							<p class="def">(<?php echo trad_lang('game_desc'); ?>)</p>
 						</li>
-						<li> <a href="culture/index.php">Reading</a> 
-							<p class="def">(After know the basic notion in N'ko, you must pratice to anderstand,
-								in this section, you will be the excellent reader I hope.</p>
+						<li> <a href="culture/index.php"><?php echo trad_lang('culture'); ?></a> 
+							<p class="def">(<?php echo trad_lang('culture_desc'); ?></p>
 						</li>
 					</ul>
-					<p class="contact_us">
-						If you have any question about anything of these section, please contact fasso administrator.
-					</p>
-					<p class="souhait">Good reading !</p>
+					
+					<p <?php align_by_lang('right','left'); ?> class="souhait"><?php echo trad_lang('souhait'); ?></p>
 				</section>
 			</div>
 		</div>
+	<!--
 	<footer dir="auto" id="copyrigth" class="rondBas ombre">&copy; ߝߊ߬ߛߏ ߞߍߦߙߐ | ߞߟߊߓߎ ߣߌ߫ ߣߊ߲߬ߝߏ߬ߦߊ &copy;</footer>
+	-->
 </body>
 </html>
